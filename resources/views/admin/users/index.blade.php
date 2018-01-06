3b0884c756e4136e8583a71a227b36e3fbc99dd5
Thiago Damasceno <thiago_d.s@hotmail.com>
2018-01-06 00:12:08 -0200

    Commit MapaLaravel

--- resources/views/admin/users/index.blade.php
+++
@@ -1,5 +0,0 @@
-@foreach($users as $user)
-	<li>{!! $user["primeiro_nome"], " " ,$user["segundo_nome"]!!}</li>
-	<li>{!! $user["primeiro_nome"]!!} {!! $user["segundo_nome"]!!} Ola mundo</li>
-
-@endforeach
